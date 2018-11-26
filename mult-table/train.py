7874a8b25b44b554bcc56ca8043d55389a8fadd0
Roman Gezikov <rgesikov@gmail.com>
2018-11-26 20:06:29 +0200

    feat: parameterization

--- mult-table/train.py
+++ mult-table/train.py
@@ -1,15 +1,27 @@
-from  datetime import datetime, timedelta
-import random
+#!/usr/bin/env python3
 
-total_examples = 10
+import argparse
+import random
+from  datetime import datetime, timedelta
+
+parser = argparse.ArgumentParser(description='Учим таблицу умножения')
+parser.add_argument('-e', dest='examples', required=True, type=int, help='Количество примеров')
+parser.add_argument('--min', dest='min', type=int, required=True, help='Минимальное число')
+parser.add_argument('--max', dest='max', type=int, required=True, help='Максимальное число')
+args = parser.parse_args()
+
+total_examples = args.examples
 
 wrong_examples = []
 total_time = timedelta()
 correct_counter = 0
 for example in range(total_examples):
     print("\nПример № {}".format(example + 1))
-    a = random.randint(2, 5)
+    a = random.randint(args.min, args.max)
     b = random.randint(2, 9)
+    if args.max % 10 != 0 and args.min % 10 != 0:
+        while (a * b) % 10 == 0:
+            b = random.randint(2, 9)
     print("Сколько будет {} * {}?".format(a, b))
     start = datetime.utcnow()
     answer = int(input("Ответ:"))
