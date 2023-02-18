744a8d227f3070a09118bef5b2068a4e8f885cfa
Roman Gezikov <rgesikov@gmail.com>
2023-02-18 15:13:50 +0200

    answer checking doesn't react to commas and spaces

--- eng-phrase/training3.py
+++ eng-phrase/training3.py
@@ -14,14 +14,18 @@ def main():
 
     max_len = max([len(x[0]) for x in questions])
 
+    total_questions = len(questions)
+    current_question = 1
     all_done = True
     for question in questions:
         question_len = len(question[0])
         question_str = question[0] + ' ' * (max_len - question_len)
-        answer = input(f"{question_str}: ")
-        correct_answer = question[1].lower()
-        if answer.strip().lower() == correct_answer:
+        answer = input(f"({current_question}/{total_questions}) {question_str}: ")
+        answer = answer.lower().strip().replace(" ", "").replace(",", "")
+        correct_answer = question[1].lower().strip().replace(" ", "").replace(",", "")
+        if answer == correct_answer:
             print("Correct!")
+            current_question += 1
         else:
             print(f"Wrong! Correct answer: {correct_answer}")
             all_done = False
