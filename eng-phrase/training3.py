55daf4b55fe0fff3b851579823e693fb7543d093
Roman Gezikov <rgesikov@gmail.com>
2023-03-23 06:42:07 +0200

    ruokasanat

--- eng-phrase/training3.py
+++ eng-phrase/training3.py
@@ -1,12 +1,19 @@
+import argparse
 import os
 import random
 import yaml
 
+def parse_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', dest='file_name', required=True, help='Input file')
+    parser.add_argument('-m', dest='mode', required=True, default='learning', help='Mode (learning, exam)')
+    args = parser.parse_args()
+    return args
 
 def main():
+    args = parse_params()
 
-    file_name = os.path.join(os.path.dirname(__file__), "imperfektikoe-1.yaml")
-    with open(file_name) as inf:
+    with open(args.file_name) as inf:
         data = yaml.safe_load(inf)
 
     questions = data['questions']
@@ -20,22 +27,30 @@ def main():
     for question in questions:
         question_len = len(question[0])
         question_str = question[0] + ' ' * (max_len - question_len)
-        answer = input(f"({current_question}/{total_questions}) {question_str}: ")
+        if args.mode == 'exam':
+            answer = input(f"({current_question}/{total_questions}) {question_str}: ")
+        else:
+            answer = input(f"{question_str} ?")
+
         answer = answer.lower().strip().replace(" ", "").replace(",", "")
         correct_answer = question[1].lower().strip().replace(" ", "").replace(",", "")
         if answer == correct_answer:
             print("Correct!")
             current_question += 1
         else:
-            # print(f"Wrong! Correct answer: {correct_answer}")
-            print(f"Wrong!")
+            if args.mode == 'learning':
+                print(f"Correct answer: {question[1]}")
+            else:
+                print(f"Wrong!")
             all_done = False
-            break
+            if args.mode == 'exam':
+                break
 
-    if not all_done:
-        print("You did NOT pass the test. Train more!")
-    else:
-        print("You pass the test SUCCESSFULLY. Well done!")
+    if args.mode == 'exam':
+        if not all_done:
+            print("You did NOT pass the test. Train more!")
+        else:
+            print("You pass the test SUCCESSFULLY. Well done!")
 
 
 if __name__ == "__main__":
